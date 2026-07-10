4edbef8bbfd02243b8c88f5814b2b868eb6f0645
Pratik Bapusaheb Khandve <khandvepratik8@gmail.com>
2026-07-10 13:31:05 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,49 @@
+from nt import system
+from tools import tools, handle_tool_call
+from context import TWIN_SYSTEM_PROMPT
+import gradio as gr
+from style import CSS, JS, EXAMPLES
+from dotenv import load_dotenv
+from openai import OpenAI
+import os
+
+
+load_dotenv(override=True)
+Gemini_API_KEY = os.getenv('Gemini_API_KEY')
+
+
+gemini = OpenAI(
+    api_key= Gemini_API_KEY,
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
+)
+MODEL_NAME = "gemini-2.5-flash-lite"
+system = [{"role": "system", "content": TWIN_SYSTEM_PROMPT}]
+
+
+def chat(message, history):
+    messages = system + history + [{"role": "user", "content": message}]
+    response = gemini.chat.completions.create(model=MODEL_NAME, messages=messages, tools=tools)
+    while response.choices[0].finish_reason == "tool_calls":
+        message = response.choices[0].message
+        tool_calls = message.tool_calls
+        results = handle_tool_call(tool_calls)
+        messages.append(message)
+        messages.extend(results)
+        response = gemini.chat.completions.create(model=MODEL_NAME, messages=messages, tools=tools)
+    return response.choices[0].message.content
+
+
+if __name__ == "__main__":
+
+    demo = gr.ChatInterface(
+        chat,
+        examples=EXAMPLES,
+        title="Digital Twin",
+        description="Talk to my AI twin about my career",
+        chatbot=gr.Chatbot(show_label=False),
+        css=CSS,
+        js=JS,
+        theme=gr.themes.Base(),
+    )
+
+    demo.launch(inbrowser=True)
---
+++ tools.py
@@ -0,0 +1,87 @@
+from dotenv import load_dotenv
+from openai import OpenAI
+import json
+import os
+import requests
+from pypdf import PdfReader
+import gradio as gr
+
+pushover_user = os.getenv("PUSHOVER_USER")
+pushover_token = os.getenv("PUSHOVER_TOKEN")
+pushover_url = "https://api.pushover.net/1/messages.json"
+
+def push(text):
+    requests.post(
+        "https://api.pushover.net/1/messages.json",
+        data={
+            "token": os.getenv("PUSHOVER_TOKEN"),
+            "user": os.getenv("PUSHOVER_USER"),
+            "message": text,
+        }
+    )
+
+def record_user_details(email, name="Name not provided", notes="not provided"):
+    push(f"Recording {name} with email {email} and notes {notes}")
+    return {"recorded": "ok"}
+
+def record_unknown_question(question):
+    push(f"Recording {question}")
+    return {"recorded": "ok"}
+
+record_user_details_json = {
+    "name": "record_user_details",
+    "description": "Use this tool to record that a user is interested in being in touch and provided an email address",
+    "parameters": {
+        "type": "object",
+        "properties": {
+            "email": {
+                "type": "string",
+                "description": "The email address of this user"
+            },
+            "name": {
+                "type": "string",
+                "description": "The user's name, if they provided it"
+            }
+            ,
+            "notes": {
+                "type": "string",
+                "description": "Any additional information about the conversation that's worth recording to give context"
+            }
+        },
+        "required": ["email"],
+        "additionalProperties": False
+    }
+}
+
+record_unknown_question_json = {
+    "name": "record_unknown_question",
+    "description": "Always use this tool to record any question that couldn't be answered as you didn't know the answer",
+    "parameters": {
+        "type": "object",
+        "properties": {
+            "question": {
+                "type": "string",
+                "description": "The question that couldn't be answered"
+            },
+        },
+        "required": ["question"],
+        "additionalProperties": False
+    }
+}
+
+tools = [{"type": "function", "function": record_user_details_json},
+        {"type": "function", "function": record_unknown_question_json}]
+tools_map = [{"record_user_details": record_user_details},
+             {"record_unknown_question": record_unknown_question}]
+
+
+def handle_tool_call(self, tool_calls):
+        results = []
+        for tool_call in tool_calls:
+            tool_name = tool_call.function.name
+            arguments = json.loads(tool_call.function.arguments)
+            print(f"Tool called: {tool_name}", flush=True)
+            tool = globals().get(tool_name)
+            result = tool(**arguments) if tool else {}
+            results.append({"role": "tool","content": json.dumps(result),"tool_call_id": tool_call.id})
+        return results
